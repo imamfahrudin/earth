4aa6c8b723972769bd3aea3134d0a7853cb46f91
Mukhammad Imam Fahrudin <fachruddin.din@gmail.com>
2023-05-23 16:08:35 +0700

    feat(earth): initial commit

---
+++ src/main/java/com/milkyway/earth/EarthApplication.java
@@ -0,0 +1,13 @@
+package com.milkyway.earth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthApplication.class, args);
+	}
+
+}
